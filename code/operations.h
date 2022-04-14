54d7a8f6323f174fb58de803fb6dd147f1278f2c
Rui Lopes <ruilopesm19@gmail.com>
2022-04-14 12:55:28 +0100

    Update operations.h

--- code/operations.h
+++ code/operations.h
@@ -10,6 +10,6 @@ typedef void (*binary)(STACK *s);
 
 void sum(STACK *s);
 void sub(STACK *s);
-void mul(STACK *s);
+void mult(STACK *s);
 void divi(STACK *s);
-void resto(STACK *s);
+void rem(STACK *s);
