7fa65a4f07b5f4aee5437f8cf317ff5deb44b849
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-04-14 22:54:11 +0100

    Add documentation files

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['create_5fstack_0',['create_stack',['../stack_8c.html#a000f06fe10503e388d8f7616b2a3b039',1,'create_stack():&#160;stack.c'],['../stack_8h.html#a000f06fe10503e388d8f7616b2a3b039',1,'create_stack():&#160;stack.c']]]
+];
