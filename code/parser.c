f73412f59e2211f4f12c5ac48a392dec37fa18fe
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-04-17 22:23:36 +0100

    Test new types in parser

--- code/parser.c
+++ code/parser.c
@@ -7,6 +7,15 @@
 #include <string.h>
 #include <assert.h>
 
+int has_point(char *token) {
+    for (int i = 0; token[i] != '\0'; i++) {
+        if (token[i] == '.') {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void handle_token(STACK *s, char *token) {
     // Se o símbolo for um operador, irá procurar a operação pretendida e realizá-la
     if (is_operator(token)) { 
@@ -14,9 +23,30 @@ void handle_token(STACK *s, char *token) {
     }
     // Caso contrário, irá ler esse elemento e de seguida adicioná-lo à stack, se possível
     else {
-        int value;
-        sscanf(token, "%d", &value); 
-        assert(push(s, value) == 0);
+        if (has_point(token)) {
+            double value;
+            sscanf(token, "%lg", &value);
+
+            STACK_ELEM new = {
+                .t = DOUBLE,
+                .data = { .d = value }
+            };
+            assert(push(s, new) == 0);
+            // Testing purposes
+            printf("A double has been pushed into the stack!\n");
+        }
+        else {
+            long value;
+            sscanf(token, "%ld", &value);
+
+            STACK_ELEM new = {
+                .t = LONG,
+                .data = { .l = value }
+            };
+            assert(push(s, new) == 0);
+            // Testing purposes
+            printf("A long has been pushed into the stack!\n");
+        }
     }
 }
 
