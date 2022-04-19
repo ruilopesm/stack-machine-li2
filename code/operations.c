c38d2c0002fb7ce5f5305e4d830b1596b7aca3b1
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2022-04-19 23:34:43 +0100

    Add converters again

--- code/operations.c
+++ code/operations.c
@@ -17,7 +17,7 @@ char get_operator(int i) {
         '|',
         ')',
         '(',
-        '~'
+        '~',
         'i',
         'f',
         'c',
@@ -63,7 +63,7 @@ void dispatch_table(STACK *s, char operator) {
         bw_or,
         increment,
         decrement,
-        bw_not
+        bw_not,
         convI,
         convD,
         convC,
@@ -360,4 +360,97 @@ void bw_not(STACK *s) {
     }
 
     push(s, result);
+}
+
+void convI(STACK *s) {
+    STACK_ELEM x;
+    
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == DOUBLE) {
+        result.t = LONG;
+        result.data.l = x.data.d;
+        push(s, result);
+    } 
+    else if (x.t == CHAR) {
+        result.t = LONG;
+        result.data.l = x.data.c;
+        push(s, result);
+    }
+    // Conversão redundante
+    else {
+        push(s, x);
+    }
+}
+
+void convD(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == LONG) {
+        result.t = DOUBLE;
+        result.data.d = x.data.l;
+        push(s, result);
+    } 
+    else if (x.t == CHAR) {
+        result.t = DOUBLE;
+        result.data.d = x.data.c;
+        push(s, result);
+    } 
+    // Conversão redundante
+    else {
+        push(s, x);
+    }
+}
+
+void convC(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == LONG) {
+        result.t = CHAR;
+        result.data.c = x.data.l % 256;
+        push(s, result);
+    } 
+    else if (x.t == STRING) {
+        result.t = CHAR;
+        result.data.c = x.data.s[0];
+        push(s, result);
+    } 
+    else {
+        push(s, x);
+    }
+}
+
+void convS(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == LONG) {
+        result.t = STRING;
+        snprintf(result.data.s, BUFSIZ, "%ld", x.data.l);
+        push(s, result);
+    } 
+    else if (x.t == DOUBLE) {
+        result.t = STRING;
+        snprintf(result.data.s, BUFSIZ, "%lg", x.data.d);
+        push(s, result);
+    } 
+    else if (x.t == CHAR) {
+        result.t = STRING;
+        result.data.s[0] = x.data.c;
+        result.data.s[1] = '\0';
+        push(s, result);
+    } 
+    else {
+        push(s, x);
+    }
 }
