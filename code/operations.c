949fee93d856567d26716a3941c2a8a42eb9287f
Filipe2817 <filipe.simoes.pereira2817@gmail.com>
2022-04-19 23:12:50 +0100

    Converters Implementados

--- code/operations.c
+++ code/operations.c
@@ -1,5 +1,5 @@
-#include "stack.h"
 #include "operations.h"
+#include "stack.h"
 
 #include <assert.h>
 #include <math.h>
@@ -18,7 +18,10 @@ char get_operator(int i) {
         // ')',
         // '(',
         // '~'
-    };
+        'i',
+        'f',
+        'c',
+        's'};
 
     return operators[i];
 }
@@ -26,10 +29,10 @@ char get_operator(int i) {
 int is_operator(char *token) {
     for (int i = 0; i < N_OPERATORS; i++) {
         if (token[0] == get_operator(i)) {
-            return 1; 
+            return 1;
         }
     }
-    
+
     return 0;
 }
 
@@ -37,7 +40,7 @@ int get_index(char operator) {
     int index = -1;
 
     for (int i = 0; i < N_OPERATORS; i++) {
-        if (operator == get_operator(i)) {
+        if (operator== get_operator(i)) {
             index = i;
         }
     }
@@ -48,7 +51,7 @@ int get_index(char operator) {
 
 void dispatch_table(STACK *s, char operator) {
     function table[] = {
-        sum, 
+        sum,
         // sub,
         // mult,
         // divi,
@@ -60,7 +63,10 @@ void dispatch_table(STACK *s, char operator) {
         // increment,
         // decrement,
         // bw_not
-    }; // As funções até agora implementadas são colocadas em posições análogas às referenciadas na função 'get_operator'.
+        convI,
+        convD,
+        convC,
+        convS}; // As funções até agora implementadas são colocadas em posições análogas às referenciadas na função 'get_operator'.
 
     int index = get_index(operator);
 
@@ -69,38 +75,35 @@ void dispatch_table(STACK *s, char operator) {
 
 void sum(STACK *s) {
     STACK_ELEM x, y;
-    
+
     assert(pop(s, &x) == 0);
     assert(pop(s, &y) == 0);
 
     STACK_ELEM result;
-    
+
     if (x.t == LONG) {
         if (y.t == DOUBLE) {
             result.t = DOUBLE;
             result.data.d = x.data.l + y.data.d;
-        }
-        else if (y.t == LONG) {
+        } else if (y.t == LONG) {
             result.t = LONG;
             result.data.l = x.data.l + y.data.l;
         }
-    } 
-    else if (x.t == DOUBLE) {
+    } else if (x.t == DOUBLE) {
         if (y.t == LONG) {
             result.t = DOUBLE;
             result.data.d = x.data.d + y.data.l;
-        }
-        else if (y.t == DOUBLE) {
+        } else if (y.t == DOUBLE) {
             result.t = DOUBLE;
             result.data.d = x.data.d + y.data.d;
-        }  
+        }
     }
     push(s, result);
 }
 
 // void sub(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &y) == 0);
 //     assert(pop(s, &x) == 0);
 
@@ -109,16 +112,16 @@ void sum(STACK *s) {
 
 // void mult(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &x) == 0);
-//     assert(pop(s, &y) == 0); 
+//     assert(pop(s, &y) == 0);
 
 //     push(s, x * y);
 // }
 
 // void divi(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &y) == 0);
 //     assert(pop(s, &x) == 0);
 
@@ -127,7 +130,7 @@ void sum(STACK *s) {
 
 // void rem(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &y) == 0);
 //     assert(pop(s, &x) == 0);
 
@@ -136,7 +139,7 @@ void sum(STACK *s) {
 
 // void power(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &y) == 0);
 //     assert(pop(s, &x) == 0);
 
@@ -145,7 +148,7 @@ void sum(STACK *s) {
 
 // void bw_xor(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &x) == 0);
 //     assert(pop(s, &y) == 0);
 
@@ -154,7 +157,7 @@ void sum(STACK *s) {
 
 // void bw_and(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &x) == 0);
 //     assert(pop(s, &y) == 0);
 
@@ -163,7 +166,7 @@ void sum(STACK *s) {
 
 // void bw_or(STACK *s) {
 //     int x, y;
-    
+
 //     assert(pop(s, &x) == 0);
 //     assert(pop(s, &y) == 0);
 
@@ -172,7 +175,7 @@ void sum(STACK *s) {
 
 // void increment(STACK *s) {
 //     int x;
-    
+
 //     assert(pop(s, &x) == 0);
 
 //     push(s, ++x);
@@ -180,7 +183,7 @@ void sum(STACK *s) {
 
 // void decrement(STACK *s) {
 //     int x;
-    
+
 //     assert(pop(s, &x) == 0);
 
 //     push(s, --x);
@@ -188,8 +191,89 @@ void sum(STACK *s) {
 
 // void bw_not(STACK *s) {
 //     int x;
-    
+
 //     assert(pop(s, &x) == 0);
 
 //     push(s, ~x);
 // }
+
+void convI(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == DOUBLE) {
+        result.t = LONG;
+        result.data.l = x.data.d;
+        push(s, result);
+    } else if (x.t == CHAR) {
+        result.t = LONG;
+        result.data.l = x.data.c;
+        push(s, result);
+    } else {
+        push(s, x);
+    }
+}
+
+void convD(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == LONG) {
+        result.t = DOUBLE;
+        result.data.d = x.data.l;
+        push(s, result);
+    } else if (x.t == CHAR) {
+        result.t = DOUBLE;
+        result.data.d = x.data.c;
+        push(s, result);
+    } else {
+        push(s, x);
+    }
+}
+
+void convC(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == LONG) {
+        result.t = CHAR;
+        result.data.c = x.data.l % 256;
+        push(s, result);
+    } else if (x.t == STRING) {
+        result.t = CHAR;
+        result.data.c = x.data.s[0];
+        push(s, result);
+    } else {
+        push(s, x);
+    }
+}
+
+void convS(STACK *s) {
+    STACK_ELEM x;
+    STACK_ELEM result;
+
+    assert(pop(s, &x) == 0);
+
+    if (x.t == LONG) {
+        result.t = STRING;
+        snprintf(result.data.s, BUFSIZ, "%ld", x.data.l);
+        push(s, result);
+    } else if (x.t == DOUBLE) {
+        result.t = STRING;
+        snprintf(result.data.s, BUFSIZ, "%lg", x.data.d);
+        push(s, result);
+    } else if (x.t == CHAR) {
+        result.t = STRING;
+        result.data.s[0] = x.data.c;
+        result.data.s[1] = '\0';
+        push(s, result);
+    } else {
+        push(s, x);
+    }
+}
