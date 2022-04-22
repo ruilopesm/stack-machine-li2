7b453e7ae6571f7bfd51891253588528ce8a14ad
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-04-22 11:21:48 +0100

    Fix warning

--- code/operations.c
+++ code/operations.c
@@ -388,11 +388,13 @@ void conv_int(STACK *s) {
         result.t = LONG;
         result.data.l = x.data.d;
         push(s, result);
-    } else if (x.t == CHAR) {
+    } 
+    else if (x.t == CHAR) {
         result.t = LONG;
         result.data.l = x.data.c;
         push(s, result);
-    } else if (x.t == STRING) {
+    } 
+    else if (x.t == STRING) {
         result.t = LONG;
         sscanf(x.data.s, "%ld", &result.data.l);
     }
@@ -413,11 +415,13 @@ void conv_double(STACK *s) {
         result.t = DOUBLE;
         result.data.d = x.data.l;
         push(s, result);
-    } else if (x.t == CHAR) {
+    } 
+    else if (x.t == CHAR) {
         result.t = DOUBLE;
         result.data.d = x.data.c;
         push(s, result);
-    } else if (x.t == STRING) {
+    } 
+    else if (x.t == STRING) {
         result.t = DOUBLE;
         sscanf(x.data.s, "%lg", &result.data.d);
     }
@@ -438,11 +442,13 @@ void conv_char(STACK *s) {
         result.t = CHAR;
         result.data.c = x.data.l % 256;
         push(s, result);
-    } else if (x.t == STRING) {
+    } 
+    else if (x.t == STRING) {
         result.t = CHAR;
         result.data.c = x.data.s[0];
         push(s, result);
-    } else {
+    } 
+    else {
         push(s, x);
     }
 }
@@ -453,21 +459,25 @@ void conv_str(STACK *s) {
     assert(pop(s, &x) == 0);
     
     STACK_ELEM result;
+    result.data.s = NULL;
 
     if (x.t == LONG) {
         result.t = STRING;
         snprintf(result.data.s, BUFSIZ, "%ld", x.data.l);
         push(s, result);
-    } else if (x.t == DOUBLE) {
+    } 
+    else if (x.t == DOUBLE) {
         result.t = STRING;
         snprintf(result.data.s, BUFSIZ, "%lg", x.data.d);
         push(s, result);
-    } else if (x.t == CHAR) {
+    } 
+    else if (x.t == CHAR) {
         result.t = STRING;
         result.data.s[0] = x.data.c;
         result.data.s[1] = '\0';
         push(s, result);
-    } else {
+    } 
+    else {
         push(s, x);
     }
 }
