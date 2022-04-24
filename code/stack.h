1fc0eeb4dcb4d344129c8a6baf3ee090140827f5
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-04-24 16:42:03 +0100

    Fix global variables

--- code/stack.h
+++ code/stack.h
@@ -31,6 +31,7 @@ typedef struct {
     int sp; /**< O stack pointer */
     int size; /**< O tamanho total da stack */
     STACK_ELEM *stc; /**< O conteúdo da stack */
+    STACK_ELEM globals[26]; /**< Variáveis "globais" */
 } STACK;
 
 /**
