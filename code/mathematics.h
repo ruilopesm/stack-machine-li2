fbfae423dec2eb7fa279bdf90803f12f13fff9d9
Mi <a100835@alunos.uminho.pt>
2022-05-10 19:40:40 +0100

    docs

--- code/mathematics.h
+++ code/mathematics.h
@@ -1,7 +1,29 @@
+/**
+ * @brief Função que executa a adiçao de dois números
+ * 
+ * @param x 1º numero a ser somado
+ * @param y 2º numero  ser somado
+ * @param result STACK_ELEMENT onde irá ser guardado o resultado
+ */
 void sum_two_numbers(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result);
 
+/**
+ * @brief Função que executa a subtração de dois números
+ * 
+ * @param x diminuendo
+ * @param y diminuidor
+ * @param result STACK_ELEMENT onde irá ser guardado o resultado
+ */
 void subtract_two_numbers(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result);
 
+
+/**
+ * @brief Função que executa a multiplicação de dois números
+ * 
+ * @param x 1º numero a ser multiplicado
+ * @param y 2º numero  ser multiplicado
+ * @param result STACK_ELEMENT onde irá ser guardado o resultado
+ */
 void multiply_two_numbers(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result);
 
 void divide_two_numbers(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result);
