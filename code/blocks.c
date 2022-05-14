68637c16d0d985eca42973d6db7aa08d300466db
Filipe2817 <filipe.simoes.pereira2817@gmail.com>
2022-05-14 17:14:31 +0100

    refactor while

--- code/blocks.c
+++ code/blocks.c
@@ -52,15 +52,15 @@ void map_string(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result, GLOBALS *g) {
 
 //Retorna 0 se x for menor ou igual a y, 1 se o contrário se verificar
 int cmpOn(STACK_ELEM x,STACK_ELEM y,STACK *temp, char *block,GLOBALS *g){
-    STACK_ELEM xp,yp;
-    assert(push(temp,x) == 0);
-    parse_line(temp,block,g);
-    assert(push(temp,y) == 0);
-    parse_line(temp,block,g);
-    assert(pop(temp,&yp) == 0);
-    assert(pop(temp,&xp) == 0);
+    STACK_ELEM xp, yp;
+    assert(push(temp, x) == 0);
+    parse_line(temp, block, g);
+    assert(push(temp, y) == 0);
+    parse_line(temp, block, g);
+    assert(pop(temp, &yp) == 0);
+    assert(pop(temp, &xp) == 0);
     if(xp.t == STRING && yp.t == STRING){
-        if(strcmp(xp.data.s,yp.data.s) <= 0){
+        if (strcmp(xp.data.s,yp.data.s) <= 0){
             return 0;
         }
         else{
@@ -83,17 +83,17 @@ int cmpOn(STACK_ELEM x,STACK_ELEM y,STACK *temp, char *block,GLOBALS *g){
     temp->sp = 0; //Esvaziar a stack temporária, caso tenha elementos anteriores de modo a não causar erros (provavelmente não necessário, mas existe como segurança extra)
 }
 
-int cmp_arrays(STACK *x, STACK *y){
+int cmp_arrays(STACK *x, STACK *y) {
     int result;
-    for(int i=0;i>=0;i++){
-        if(x->stc[i].t==STRING && y->stc[i].t == STRING){
-            result = (strcmp(x->stc[i].data.s,y->stc[i].data.s));
+    for(int i = 0; i >= 0; i++){
+        if (x->stc[i].t == STRING && y->stc[i].t == STRING) {
+            result = (strcmp(x->stc[i].data.s, y->stc[i].data.s));
         }
-        else if(x->stc[i].t==ARRAY && y->stc[i].t == ARRAY){
-            result = cmp_arrays(x->stc[i].data.a,y->stc[i].data.a);
+        else if (x->stc[i].t == ARRAY && y->stc[i].t == ARRAY) {
+            result = cmp_arrays(x->stc[i].data.a, y->stc[i].data.a);
         }
         else{
-            double xc = get_double_arg(x->stc[i]),yc = get_double_arg(y->stc[i]);
+            double xc = get_double_arg(x->stc[i]), yc = get_double_arg(y->stc[i]);
             result = xc - yc;
         }
         if (result < 0){
@@ -103,10 +103,10 @@ int cmp_arrays(STACK *x, STACK *y){
             return 1;
         }
         //Se chegar ao fim de qualquer um dos arrays termina a comparação
-        if(i==x->sp-1){
+        if(i == x->sp-1){
             return 0;
         }
-        if(i==y->sp-1){
+        if(i == y->sp-1){
             return 1;
         }
     }
@@ -116,12 +116,12 @@ int cmp_arrays(STACK *x, STACK *y){
 //Ordena um array segundo dada condição por um bloco
 void sortOn(STACK_ELEM block,STACK_ELEM *array,GLOBALS *g){
     STACK *temp=create_stack();
-    int i,j;
-    for(i=1;i<array->data.a->sp;i++){ 
-        for(j=0;j<i && cmpOn(array->data.a->stc[i],array->data.a->stc[j],temp,block.data.s,g);j++){
+    int i, j;
+    for(i = 1; i < array->data.a->sp; i++){ 
+        for(j = 0; j < i && cmpOn(array->data.a->stc[i], array->data.a->stc[j], temp, block.data.s, g); j++){
         }
-        for(;j<i;j++){
-            swap(array->data.a,j,i);
+        for(; j < i; j++){
+            swap(array->data.a, j, i);
         }
     }
 }
@@ -200,16 +200,23 @@ void filter_string(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result, GLOBALS *g) {
 
 void while_operation(STACK *s, STACK_ELEM x, GLOBALS *g) {
     STACK_ELEM aux;
-    int flag = 1;
-
-    while (flag) {
+    
+    do {
         parse_line(s, x.data.b, g);
         assert(pop(s, &aux) == 0);
-        if (aux.data.l == 0) {
-            flag = 0;
-        }
-        else {
-            push(s, aux);
-        }
-    }
+    } while (truthy_value(aux));
+}
+
+int truthy_value(STACK_ELEM x) {
+
+    if (x.t == ARRAY && x.data.a->sp)
+        return 1;
+    else if (x.t == BLOCK && x.data.b)
+        return 1;
+    else if (x.t == STRING && x.data.s)
+        return 1;
+    else if (get_double_arg(x))
+        return 1;
+
+    return 0;
 }
--- code/blocks.h
+++ code/blocks.h
@@ -18,3 +18,5 @@ void filter_string(STACK_ELEM x, STACK_ELEM y, STACK_ELEM *result, GLOBALS *g);
 
 void while_operation(STACK *s, STACK_ELEM x, GLOBALS *g);
 
+int truthy_value(STACK_ELEM x);
+
